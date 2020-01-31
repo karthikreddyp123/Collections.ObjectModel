a60e46a338d48149d4842782d3d3143de44db0d6
Karthik Patlolla <Karthik_Patlolla@epam.com>
2020-01-31 14:39:13 +0530

    Add project files.

---
+++ Collections.ObjectModel/ReadOnlyCollection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.ObjectModel
+{
+    class ReadOnlyCollection
+    {
+        static void Main()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(3);
+            list.Add(5);
+
+            // Constructor.
+            ReadOnlyCollection<int> readOnlyCollection = new ReadOnlyCollection<int>(list);
+
+            // Loop over ReadOnlyCollection.
+            foreach (int value in readOnlyCollection)
+            {
+                Console.WriteLine("read: {0}", value);
+            }
+
+            // Copy ReadOnlyCollection to an array.
+            int[] array = new int[3];
+            readOnlyCollection.CopyTo(array, 0);
+
+            // Display array.
+            foreach (int value in array)
+            {
+                Console.WriteLine("array: {0}", value);
+            }
+
+            // Use methods on ReadOnlyCollection.
+            int count = readOnlyCollection.Count;
+            bool contains = readOnlyCollection.Contains(-1);
+            int index = readOnlyCollection.IndexOf(3);
+            //Append operation doesnot actully append it to the collection
+            readOnlyCollection.Append(100);
+            Console.WriteLine("{0}, {1}, {2}", count, contains, index);
+            Console.WriteLine("After Appending 100:");
+            foreach (int value in readOnlyCollection)
+            {
+                Console.WriteLine("read: {0}", value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
